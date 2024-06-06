96532109d3129f277991a10dd8fcc01a19f572c2
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-06-06 15:22:08 +0530

    help.py

--- X/modules/user/help.py
+++ X/modules/user/help.py
@@ -126,7 +126,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Japanese_Userbot_Support × Mᴏᴅᴜʟᴇꜱ Pʀᴏᴠɪᴅᴇᴅ Bʏ 𝐇𝐞𝐚𝐫𝐭𝐁𝐞𝐚𝐭-𝐗-𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭� •"
+            message, f"```{str(ac)}```\n• @HeartBeat_Muzic × Mᴏᴅᴜʟᴇꜱ Pʀᴏᴠɪᴅᴇᴅ Bʏ 𝐇𝐞𝐚𝐫𝐭𝐁𝐞𝐚𝐭-𝐗-𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭� •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
